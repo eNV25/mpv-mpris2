8b331680577fac17b6ab1508a199bd374f5b43b9
eNV25 <env252525@gmail.com>
2024-05-15 11:34:48 +0200

    build.rs: update to not use case-insensitive regex

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 use std::{env, error, path::Path};
 
 fn main() -> Result<(), Box<dyn error::Error>> {
-    const MPV_SYMBOLS: &str = "(?i:mpv_).*";
+    const MPV_SYMBOLS: &str = "(MPV|mpv)_.*";
     let header = Path::new(&pkg_config::get_variable("mpv", "includedir")?).join("mpv/client.h");
     let header = <&str>::try_from(header.as_os_str())?;
     let output = Path::new(&env::var("OUT_DIR")?).join("ffi.rs");
@@ -9,13 +9,10 @@ fn main() -> Result<(), Box<dyn error::Error>> {
         .header(header)
         .clang_arg("-Wp,-D_FORTIFY_SOURCE=2")
         .clang_arg("-Wp,-DMPV_ENABLE_DEPRECATED=0")
-        .impl_debug(true)
-        .allowlist_var(MPV_SYMBOLS)
-        .allowlist_type(MPV_SYMBOLS)
-        .allowlist_function(MPV_SYMBOLS)
+        .opaque_type("mpv_handle")
+        .allowlist_item(MPV_SYMBOLS)
         .constified_enum(MPV_SYMBOLS)
         .prepend_enum_name(false)
-        .opaque_type("mpv_handle")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         .generate()?
         .write_to_file(output)?;
