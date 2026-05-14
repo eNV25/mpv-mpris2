18f5adad74bddc5098966424dbccd87151cb4795
eNV25 <env252525@gmail.com>
2026-05-15 00:40:15 +0200

    src/main: Remove smol-macros and macro_rules_attribute dependencies
    Manually initialize `smol::Executor` and call `smol::block_on` in `main()` instead of using the `smol_macros::main` macro.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 use crate::{mpv::Mpv, plugin::Player};
-use macro_rules_attribute::apply;
 use mpris_server::Server;
 use smol::Executor;
-use smol_macros::main;
 use tracing::Level;
 use tracing_subscriber::EnvFilter;
 
@@ -10,22 +8,24 @@ mod common;
 mod mpv;
 mod plugin;
 
-#[apply(main!)]
-async fn main(ex: &Executor) -> anyhow::Result<()> {
+fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt()
         .with_env_filter(EnvFilter::from_default_env().add_directive(Level::INFO.into()))
         .init();
 
-    let (mpv, events_tx) = Mpv::new(ex, plugin::args::mpv_ipc_fd()?.try_into()?);
+    let ex = Executor::new();
+    smol::block_on(ex.run(async {
+        let (mpv, events_tx) = Mpv::new(&ex, plugin::args::mpv_ipc_fd()?.try_into()?);
 
-    let Some(pid): Option<usize> = mpv.get_property("pid").await? else {
-        anyhow::bail!("No PID found");
-    };
+        let Some(pid): Option<usize> = mpv.get_property("pid").await? else {
+            anyhow::bail!("No PID found");
+        };
 
-    let name = format!("mpv.instance{}", pid);
-    let server = Server::new(&name, Player::new(mpv).await?).await?;
+        let name = format!("mpv.instance{}", pid);
+        let server = Server::new(&name, Player::new(mpv).await?).await?;
 
-    plugin::main_loop(ex, server, events_tx).await?;
+        plugin::main_loop(&ex, server, events_tx).await?;
 
-    Ok(())
+        Ok(())
+    }))
 }
